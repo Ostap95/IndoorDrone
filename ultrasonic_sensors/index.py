c9c7932c9425481adf17ef8a121ff77136c339f6
= <ostap_95@hotmail.com>
2019-09-05 19:21:19 +0100

    improv

--- ultrasonic_sensors/index.py
+++ ultrasonic_sensors/index.py
@@ -6,24 +6,17 @@ import json
 
 SERVER_ENDPOINT = "http://192.168.1.7:3000/sensor_data"
 
-sensor_front = DistanceSensor(echo=5, trigger=4)
-sensor_right = DistanceSensor(echo=6, trigger=17)
-sensor_top = DistanceSensor(echo=13, trigger=27)
-sensor_left = DistanceSensor(echo=19, trigger=22)
-sensor_back = DistanceSensor(echo=26, trigger=23)
+sensor_front = DistanceSensor(echo=18, trigger=17)
+sensor_right = DistanceSensor(echo=26, trigger=19)
+sensor_left = DistanceSensor(echo=13, trigger=6)
+sensor_back = DistanceSensor(echo=21, trigger=20)
 
 while True:
-    # print("Front distance: ", sensor_front.distance)
-    # print("Back distance: ", sensor_back.distance)
-    # print("Left distance: ", sensor_left.distance)
-    # print("Right distance: ", sensor_right.distance)
-    # print("Top distance: ", sensor_top.distance)
-
-    sensor_data = {
-        'front':round(sensor_front.distance * 100, 2),
-        'back':round(sensor_back.distance * 100, 2),
-        'left':round(sensor_left.distance * 100, 2),
-        'right':round(sensor_right.distance * 100, 2),
-        'top':round(sensor_top.distance * 100, 2)
-    }
-    requests.post(SERVER_ENDPOINT, headers = {u'content-type': u'application/json'}, data = json.dumps(sensor_data))
+	sensor_data = {
+			'front':round(sensor_front.distance * 100, 2),
+			'back':round(sensor_back.distance * 100, 2),
+			'left':round(sensor_left.distance * 100, 2),
+			'right':round(sensor_right.distance * 100, 2),
+	}
+	requests.post(SERVER_ENDPOINT, headers = {u'content-type': u'application/json'}, data = json.dumps(sensor_data))
+	sleep(0.3)
